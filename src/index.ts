805cfac9a719e5a23dfb03cd31ef7065362a7cae
lpenaud <l.penaud@zaclys.net>
2020-02-03 12:09:09 +0100

    Add first route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("non");
+import * as express from "express";
+import router from "./routes";
+
+const port = Number(process.env.PORT) || 8080;
+const hostname = process.env.hostname || "localhost"
+const app = express();
+
+app.use('/api', router);
+
+app.listen(port, hostname, () => {
+    console.log(`Listening http://${hostname}:${port}`);
+});
+
