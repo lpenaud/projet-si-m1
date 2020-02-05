8b45bcc10e944b40719f4a88df454f1babe689e0
lpenaud <l.penaud@zaclys.net>
2020-02-05 09:59:41 +0100

    Add boot.sh

---
+++ boot.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: <hostname> <port>"
+    exit 1
+fi
+
+url="http://$1:$2"
+
+curlApi() {
+    local urlApi="$url/api/$2"
+    echo curl -d \"$1\" -X POST $urlApi
+    curl -d "$1" -X POST $urlApi
+}
+
+curlBook() {
+    curlApi "$1" book
+}
+
+curlBook "title=Le Guide du voyageur galactique&author=John Ronald Reuel Tolkien&genre=Fantasy"
+curlBook "title=Le Seigneur des anneaux&author=John Ronald Reuel Tolkien&genre=Science-fiction"
