ae17feeeda4608765d3c7bfc9777f42d89bfcfa6
lpenaud <l.penaud@zaclys.net>
2020-02-11 19:17:28 +0100

    Fix: book model type

--- lib/models.d.ts
+++ lib/models.d.ts
@@ -1,5 +1,6 @@
 export interface Book {
-    _id: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    _id: any;
     title: string;
     author: string;
     genre?: string;
