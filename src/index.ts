ac9d115eb103d6efe7253e3ca49ca2fe2304472e
lpenaud <l.penaud@zaclys.net>
2020-02-14 16:43:03 +0100

    Remove body-parser

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import * as express from "express";
 import router from "./routes";
 import { hostname, port } from "./config";
 import * as cors from "cors";
-import bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({ allowedHeaders: "http://localhost:3000" }))
-app.use(bodyParser());
-app.use('/api', router);
-
-app.listen(port, hostname, () => {
+app
+  .use(cors({ allowedHeaders: "http://localhost:3000" }))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use('/api', router)
+  .listen(port, hostname, () => {
     console.log(`Listening http://${hostname}:${port}`);
-});
+  });
 
