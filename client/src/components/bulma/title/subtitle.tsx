13bbaec8fb60c2e0dbda18edfc3a303e7d7b4912
lpenaud <l.penaud@zaclys.net>
2020-02-16 22:54:39 +0100

    Add title components

---
+++ client/src/components/bulma/title/subtitle.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import classNames from "classnames";
+import { TitleLevel } from "../modifiers";
+
+interface SubtitleProps {
+  level?: TitleLevel;
+}
+
+export default class Subtitle extends React.Component<SubtitleProps> {
+  render() {
+    const className = classNames({
+      'subtitle': true,
+      [`is-${this.props.level}`]: this.props.level,
+    });
+    return (
+      <p className={className}>
+        {this.props.children}
+      </p>
+    )
+  }
+}
---
+++ client/src/components/bulma/title/title.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { TitleLevel } from "../modifiers";
+import classNames from "classnames";
+
+interface TitleProps {
+  level?: TitleLevel;
+  isSpaced?: boolean;
+}
+
+export default class Title extends React.Component<TitleProps> {
+  render() {
+    const className = classNames({
+      'title': true,
+      [`is-${this.props.level}`]: this.props.level,
+      'is-spaced': this.props.isSpaced,
+    });
+    return (
+      <p className={className}>
+        {this.props.children}
+      </p>
+    )
+  }
+}
