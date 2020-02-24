dbee8a112cc06d23d83358f79709ecaecbe584b8
lpenaud <l.penaud@zaclys.net>
2020-02-24 01:49:47 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -8,15 +8,36 @@ fi
 
 url="http://$1:$2"
 
+# Route Method ...key-value
 curlApi() {
-    local urlApi="$url/api/$2"
-    echo curl -d \"$1\" -X POST $urlApi
-    curl -d "$1" -X POST $urlApi
-    echo ""
+    local urlApi="$url/api/$1"
+    local method="${2}"
+    local cmd=""
+    shift
+    shift
+    if [ $# -ge 1 ]
+    then
+        local i=0
+        local data="${1}"
+        shift
+        while [ $# -ne 0 ]
+        do
+            if [ $((i%2)) -eq 0 ]
+            then
+                data="${data}=${1}"
+            else
+                data="${data}&${1}"
+            fi
+            i=$((i+1))
+            shift
+        done
+        cmd="curl -d ${data} -X ${method} ${urlApi}"
+    else
+        cmd="curl -X ${method} ${urlApi}"
+    fi
+    echo $cmd
+    echo $(${cmd})
 }
 
-curlLearner() {
-    curlApi "$1" learner
-}
-
-curlLearner "name=Patrick&lastName=Sebastien&email=patrick.sebastien@email.com"
+curlApi trainer POST name Patrick lastName Sebastien email 'patrick.sebastien@email.com'
+curlApi lesson POST name BDD idTrainer 1
